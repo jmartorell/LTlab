e75fb3cfa76ceb05a1d272845f436e54c92fcc58
Juan Martorell <jmartorell@users.sourceforge.net>
2014-08-26 08:48:29 +0200

    Fixed destination file name

--- testes-da.sh
+++ testes-da.sh
@@ -23,7 +23,7 @@ for file in $FILELIST
 		echo "Processing $file"
 		java -jar ./dist/languagetool-commandline.jar --language es -v \
 		--enable EN_BASE_A \
-		 $file >/dev/null 2>logs-new/${file:6}.log
+		 $file >/dev/null 2>logs-new/${file:6}.da.log
 	}
 }
 
