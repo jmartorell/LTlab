ad955c200b247e6be7fd8e5f00beee3ad5e30c6c
Juan Martorell <jmartorell@users.sourceforge.net>
2016-04-03 20:57:59 +0200

    Added support for wikipediacheck

--- relink.sh
+++ relink.sh
@@ -2,4 +2,6 @@
 # Refreshes link to distributed version of LT.
 # languagetool link need to be present
 rm dist
+rm languagetool-wikipedia.jar
 ln -s languagetool/languagetool-standalone/target/LanguageTool-*/* dist
+ln -s languagetool/languagetool-wikipedia/target/LanguageTool*/LanguageTool*/languagetool-wikipedia.jar
---
+++ stestes-wiki.sh
@@ -0,0 +1,2 @@
+#java -jar wikitest/languagetool-wikipedia.jar check-data --file texts/eswiki-latest-pages-articles.xml --language es
+java -jar languagetool-wikipedia.jar check-data --file texts/eswiki-latest-pages-articles.xml --language es
